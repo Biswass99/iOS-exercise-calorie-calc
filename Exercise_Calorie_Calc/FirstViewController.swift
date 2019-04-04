39ca96f75cccbf9fcf0a31514b742b8ccb7c50e9
user150447 <user150447@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-04-04 14:48:16 -0500

    Added Segment Control and number keypad

--- Exercise_Calorie_Calc/FirstViewController.swift
+++ Exercise_Calorie_Calc/FirstViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 var userAge: Double?
 var userWeight: Double?
 var userGender: String = "Female"
+var userWeightUnit: String = "Pounds"
 
 
 class FirstViewController: UIViewController, UITextFieldDelegate{
@@ -27,6 +28,20 @@ class FirstViewController: UIViewController, UITextFieldDelegate{
     
     @IBOutlet weak var yourWeight: UITextField!
     
+    @IBOutlet weak var weightButton: UISegmentedControl!
+    
+    @IBAction func weightButton(_ sender: Any) {
+        switch weightButton.selectedSegmentIndex {
+        case 0:
+            userWeightUnit = "Pounds"
+        case 1:
+            userWeightUnit = "Kilos"
+        default:
+            return
+        }
+    }
+    
+    
     @IBOutlet weak var genderButton: UISegmentedControl!
     
     @IBAction func genderButton(_ sender: Any) {
@@ -39,10 +54,32 @@ class FirstViewController: UIViewController, UITextFieldDelegate{
            return
         }
     }
-    // userWeight is in pounds, so users must input their weight in pounds
+    // formula uses userWeight in pounds
+    // Weight entered by user must be converted to pounds
     
     @IBAction func savePersonalInfo(_ sender: Any) {
-        userWeight = Double(yourWeight.text!)
+        
+        
+        
+          if let weightNumber = Double(yourWeight.text!) {
+            switch userWeightUnit {
+            case "Pounds":
+                // user entered weight is in pounds
+                
+                userWeight = Double(weightNumber)
+                
+                
+            case "Kilos":
+                // user entered weight is in kilograms
+                // convert weight in kilograms to pounds
+                
+                userWeight = Double(weightNumber * 2.204)
+                
+            default:
+                return
+            } // end switch userWeightUnit
+        }// end if
+
         userAge = Double(yourAge.text!)
     }
     // function to hide the virtual keyboard
